5cac6ff6e14bc4cf2e5e6da62b56e58f8fa16a6b
pedrope2206 <pivettapedro93@gmail.com>
2022-05-27 16:44:18 -0300

    AMS_EX_5

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿double C = 0;
+double F = 0;
+string nome = "";
+
+
+Console.WriteLine("Olá qual é o seu nome?");
+nome = Console.ReadLine();
+
+Console.WriteLine($"Olá, {nome} quantos gráus fahrenheit quer converter?" );
+F = Convert.ToDouble(Console.ReadLine());
+
+C = (F -32) * 5 / 9;
+
+Console.WriteLine($"{nome}, {F} gráus fahrenheit valem {C}  gráus célcius");
+
